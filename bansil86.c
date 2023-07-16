272326760909cd80f1397a4225e2921f0c2b9649
bansilghori1 <136827896+bansilghori1@users.noreply.github.com>
2023-07-16 19:30:43 +0630

    first commit

---
+++ bansil86.c
@@ -0,0 +1,33 @@
+/*Q.1 Write a Program to convert the given string in uppercase without using any string function.
+For example,
+Input:
+Enter any string: hello world
+
+Output:
+Uppercase string: HELLO WORLD
+*/
+#include<stdio.h>
+
+void main (){
+
+int a[100];
+int i;
+printf("Enter the string :  ");
+scanf("%s",a);
+
+
+for(i=0;i=100;i++){
+
+
+
+
+
+
+
+
+
+
+
+}
+
+}
---
+++ bansil88.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main (){
+
+int a[100];
+int i;
+printf("Enter the string :  ");
+scanf("%s",a);
+
+
+for(i=0;i=100;i++){}}
