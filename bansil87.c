8f11a03c994fc4231ca0fe068618fbbf5d58006b
bansilghori1 <136827896+bansilghori1@users.noreply.github.com>
2023-07-18 13:40:06 +0630

    commit

--- bansil87.c
+++ bansil87.c
@@ -1,11 +1,28 @@
+//Q2.convert into lower case.
+
+
 #include<stdio.h>
 
 void main (){
 
-int a[100];
-int i;
-printf("Enter the string :  ");
-scanf("%s",a);
+int n,i;
 
+printf("Enter the string length =  ");
+scanf("%d",&n);
 
-for(i=0;i=100;i++){}}
+char a[n];
+
+printf("enter your senteces (without any space) : ");
+scanf("%s",&a);
+
+for(i=0;i<n;i++){
+    if(a[i]>=65 && a[i]<=90){
+
+    a[i]=a[i] + 32;
+    }
+}
+    
+    
+    printf("The converted text in lower case is :  %s",a);
+
+}
