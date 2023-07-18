3cfee008c18630840d7019f830355948f62d08ae
bansilghori1 <136827896+bansilghori1@users.noreply.github.com>
2023-07-18 13:40:47 +0630

    commit

--- bansil88.c
+++ bansil88.c
@@ -1,11 +1,37 @@
+/*Q.3 Write a Program to convert the given string in toggle case without using any string function.
+For example,
+Input:
+Enter any string: HelLo worLD
+
+Output:
+Toggle case string: hELlO WORld
+*/
 #include<stdio.h>
 
 void main (){
 
-int a[100];
-int i;
-printf("Enter the string :  ");
-scanf("%s",a);
+int n,i;
 
+printf("Enter the string length =  ");
+scanf("%d",&n);
 
-for(i=0;i=100;i++){}}
+char a[n];
+
+printf("enter your senteces (without any space) : ");
+scanf("%s",&a);
+
+for(i=0;i<n;i++){
+    if(a[i]>=65 && a[i]<=90){
+
+    a[i]=a[i] + 32;
+    }else{
+
+      a[i]=a[i] - 32;
+
+    }
+}
+    
+    
+    printf("The converted text toggle case is :%s",a);
+
+}
